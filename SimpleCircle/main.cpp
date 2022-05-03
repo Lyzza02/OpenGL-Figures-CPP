b22bcc718d6573e4b23efb16c15fe789950c6d07
Elyzza Jane Abrigo <elyzza.abrigo@gmail.com>
2022-05-03 17:51:15 +0800

    [MOD] added circles

--- SimpleCircle/main.cpp
+++ SimpleCircle/main.cpp
@@ -1,11 +1,14 @@
 #include<GL/glut.h>
 #include<iostream>
+#include<math.h>
+#define PI 3.141592653589793238462643383279502884197
 
 using namespace std;
 
 void display();
 void x_Axis();
 void y_Axis();
+void circle(GLfloat x, GLfloat y, GLfloat radius);
 
 int main(int argc, char** argv){
    glutInit(&argc, argv);
@@ -22,11 +25,23 @@ void display(){
 	x_Axis();
 	y_Axis();
 	
+	//center
+	glColor3ub(196, 221, 255);
+	circle(0.0f, 0.0f, .5f);
+	
+	//right
+	glColor3ub(247, 255, 147);
+	circle(0.4f, 0.4f, .2f);
+	
+	//left
+	glColor3ub(161, 227, 216);
+	circle(-0.4f, 0.4f, .2f);
+	
 	glFlush();
 }
 
 void x_Axis(){
-	glColor3ub(254, 0, 0);
+	glColor3ub(68, 106, 70);
 	glBegin(GL_LINES);
 	glVertex3f(-1.0, 0.0, 0.0);
 	glVertex3f(1.0, 0.0, 0.0);
@@ -34,10 +49,28 @@ void x_Axis(){
 }
 
 void y_Axis(){
-	glColor3ub(31, 255, 0);
+	glColor3ub(0, 29, 110);
 	glBegin(GL_LINES);
 	glVertex3f(0.0, -1.0, 0.0);
 	glVertex3f(0.0, 1.0, 0.0);
+	glEnd();
+}
+
+void circle(GLfloat x, GLfloat y, GLfloat radius){
+	int triangleAmount = 40;
+	
+	GLfloat twicePi = 2.0f * PI;
+	
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex2f(x, y);
+	
+	for (int i = 0; i<=triangleAmount ; i++){
+		glVertex2f(
+			x + (radius * cos(i*twicePi / triangleAmount)),
+			y + (radius * sin(i*twicePi / triangleAmount))
+		);
+	}
+	
 	glEnd();
 }
 
