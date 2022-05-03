5bf420d11460adb353b09afe252977aa89eff2ca
Elyzza Jane Abrigo <elyzza.abrigo@gmail.com>
2022-05-03 17:23:13 +0800

    [ADD] x and y axis lines

--- SimpleCircle/main.cpp
+++ SimpleCircle/main.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 
 void display();
+void x_Axis();
+void y_Axis();
 
 int main(int argc, char** argv){
    glutInit(&argc, argv);
@@ -14,8 +16,28 @@ int main(int argc, char** argv){
 
 
 void display(){
+	glClearColor(1.0,1.0,1.0,1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 	
+	x_Axis();
+	y_Axis();
+	
 	glFlush();
+}
+
+void x_Axis(){
+	glColor3ub(254, 0, 0);
+	glBegin(GL_LINES);
+	glVertex3f(-1.0, 0.0, 0.0);
+	glVertex3f(1.0, 0.0, 0.0);
+	glEnd();
+}
+
+void y_Axis(){
+	glColor3ub(31, 255, 0);
+	glBegin(GL_LINES);
+	glVertex3f(0.0, -1.0, 0.0);
+	glVertex3f(0.0, 1.0, 0.0);
+	glEnd();
 }
 
