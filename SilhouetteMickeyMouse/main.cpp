583941c1e83805348c68d0948f0c4cefcf56dd75
Elyzza Jane Abrigo <elyzza.abrigo@gmail.com>
2022-05-04 08:06:16 +0800

    [ADD] silhouette mickey mouse files

---
+++ SilhouetteMickeyMouse/main.cpp
@@ -0,0 +1,71 @@
+#include<GL/glut.h>
+#include<iostream>
+#include<math.h>
+#define PI 3.141592653589793238462643383279502884197
+
+using namespace std;
+
+void display();
+void x_Axis();
+void y_Axis();
+void circle(GLfloat x, GLfloat y, GLfloat radius);
+
+int main(int argc, char** argv){
+   glutInit(&argc, argv);
+   glutCreateWindow("Silhouette Mickey Mouse");
+   glutDisplayFunc(display);
+   glutMainLoop();
+}
+
+
+void display(){
+	glClearColor(1.0,1.0,1.0,1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	
+	//center
+	glColor3ub(0,0,0);
+	circle(0.0f, 0.0f, .5f);
+	
+	//right
+	circle(0.45f, 0.45f, .35f);
+	
+	//left
+	circle(-0.45f, 0.45f, .35f);
+	
+	glFlush();
+}
+
+void x_Axis(){
+	glColor3ub(68, 106, 70);
+	glBegin(GL_LINES);
+	glVertex3f(-1.0, 0.0, 0.0);
+	glVertex3f(1.0, 0.0, 0.0);
+	glEnd();
+}
+
+void y_Axis(){
+	glColor3ub(0, 29, 110);
+	glBegin(GL_LINES);
+	glVertex3f(0.0, -1.0, 0.0);
+	glVertex3f(0.0, 1.0, 0.0);
+	glEnd();
+}
+
+void circle(GLfloat x, GLfloat y, GLfloat radius){
+	int triangleAmount = 40;
+	
+	GLfloat twicePi = 2.0f * PI;
+	
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex2f(x, y);
+	
+	for (int i = 0; i<=triangleAmount ; i++){
+		glVertex2f(
+			x + (radius * cos(i*twicePi / triangleAmount)),
+			y + (radius * sin(i*twicePi / triangleAmount))
+		);
+	}
+	
+	glEnd();
+}
+
